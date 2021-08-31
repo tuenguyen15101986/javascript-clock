0d66b0a53274ac96d19e2aa3230fc03cbe7e38be
tuenguyen15101986 <tuenguyen15101986@outlook.com>
2021-08-31 22:08:22 +0700

    210831

--- App.js
+++ App.js
@@ -1,18 +1,38 @@
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      breakLength: 5,
+      sessionLength: 25,
+      timeLeft: 25,
+    };
+  }
   render() {
     return (
       <div id="app">
-        <div className="config-wrapper">
+        <div id="config-wrapper">
           <div className="config">
             <div id="break-label">Break Length</div>
+            <button id="break-decrement">Decrement</button>
+            <div id="break-length">{this.state.breakLength}</div>
+            <button id="break-increment">Increment</button>
           </div>
           <div className="config">
             <div id="session-label">Session Length</div>
+            <button id="session-decrement">Decrement</button>
+            <div id="session-length">{this.state.sessionLength}</div>
+            <button id="session-increment">Increment</button>
           </div>
         </div>
+        <div id="timer-wrapper">
+          <div id="timer-label">Session</div>
+          <div id="time-left">{this.state.timeLeft}</div>
+          <button id="start_stop">Start / Stop</button>
+          <button id="reset">Reset</button>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-ReactDOM.render(<App/>, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"));
