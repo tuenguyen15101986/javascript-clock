7fe64e075911ddfff11384040cf6cac658ba6115
tuenguyen15101986 <tuenguyen15101986@outlook.com>
2021-09-01 21:18:40 +0700

    210901

--- App.js
+++ App.js
@@ -4,32 +4,53 @@ class App extends React.Component {
     this.state = {
       breakLength: 5,
       sessionLength: 25,
-      timeLeft: 25,
+      countDown: 25,
+      startStop: "Start"
     };
   }
   render() {
     return (
       <div id="app">
+        <h1>Pomodoro Timer</h1>
         <div id="config-wrapper">
           <div className="config">
-            <div id="break-label">Break Length</div>
-            <button id="break-decrement">Decrement</button>
-            <div id="break-length">{this.state.breakLength}</div>
-            <button id="break-increment">Increment</button>
+            <div className="label" id="break-label">
+              Break Length
+            </div>
+            <div className="control">
+              <button id="break-decrement">-</button>
+              <div className="time-display" id="break-length">
+                {this.state.breakLength}
+              </div>
+              <button id="break-increment">+</button>
+            </div>
           </div>
           <div className="config">
-            <div id="session-label">Session Length</div>
-            <button id="session-decrement">Decrement</button>
-            <div id="session-length">{this.state.sessionLength}</div>
-            <button id="session-increment">Increment</button>
+            <div className="label" id="session-label">
+              Session Length
+            </div>
+            <div className="control">
+              <button id="session-decrement">-</button>
+              <div className="time-display" id="session-length">
+                {this.state.sessionLength}
+              </div>
+              <button id="session-increment">+</button>
+            </div>
           </div>
         </div>
         <div id="timer-wrapper">
-          <div id="timer-label">Session</div>
-          <div id="time-left">{this.state.timeLeft}</div>
-          <button id="start_stop">Start / Stop</button>
-          <button id="reset">Reset</button>
+          <div className="label" id="timer-label">
+            Session
+          </div>
+          <div className="time-display" sid="time-left">
+            {this.state.countDown}
+          </div>
+          <div className="control">
+            <button id="start_stop">{this.state.startStop}</button>
+            <button id="reset">Reset</button>
+          </div>
         </div>
+        <footer>A project by Tue Nguyen</footer>
       </div>
     );
   }
