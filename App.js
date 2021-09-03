1ea25a4630a3a8514bbedbb75f1f590d2945835e
tuenguyen15101986 <tuenguyen15101986@outlook.com>
2021-09-03 22:31:13 +0700

    210903

--- App.js
+++ App.js
@@ -5,9 +5,31 @@ class App extends React.Component {
       breakLength: 5,
       sessionLength: 25,
       countDown: 25,
-      startStop: "Start"
+      startStop: "Start",
     };
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
   }
+  handleDecrement = (event) => {
+    if (event.target.id === "break-decrement" && this.state.breakLength > 0) {
+      this.setState({ breakLength: (this.state.breakLength -= 1) });
+    } else if (
+      event.target.id === "session-decrement" &&
+      this.state.sessionLength > 0
+    ) {
+      this.setState({ sessionLength: (this.state.sessionLength -= 1) });
+    }
+  };
+  handleIncrement = (event) => {
+    if (event.target.id === "break-increment" && this.state.breakLength < 60) {
+      this.setState({ breakLength: (this.state.breakLength += 1) });
+    } else if (
+      event.target.id === "session-increment" &&
+      this.state.sessionLength < 60
+    ) {
+      this.setState({ sessionLength: (this.state.sessionLength += 1) });
+    }
+  };
   render() {
     return (
       <div id="app">
@@ -18,11 +40,15 @@ class App extends React.Component {
               Break Length
             </div>
             <div className="control">
-              <button id="break-decrement">-</button>
+              <button id="break-decrement" onClick={this.handleDecrement}>
+                -
+              </button>
               <div className="time-display" id="break-length">
                 {this.state.breakLength}
               </div>
-              <button id="break-increment">+</button>
+              <button id="break-increment" onClick={this.handleIncrement}>
+                +
+              </button>
             </div>
           </div>
           <div className="config">
@@ -30,11 +56,15 @@ class App extends React.Component {
               Session Length
             </div>
             <div className="control">
-              <button id="session-decrement">-</button>
+              <button id="session-decrement" onClick={this.handleDecrement}>
+                -
+              </button>
               <div className="time-display" id="session-length">
                 {this.state.sessionLength}
               </div>
-              <button id="session-increment">+</button>
+              <button id="session-increment" onClick={this.handleIncrement}>
+                +
+              </button>
             </div>
           </div>
         </div>
